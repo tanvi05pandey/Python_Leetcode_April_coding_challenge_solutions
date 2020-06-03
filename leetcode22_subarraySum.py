2f1d1d7c4904df18fa5db44bb94026f9f1466731
tanvi05pandey <tanvi05pandey@gmail.com>
2020-06-03 15:16:03 -0700

    Update leetcode22_subarraySum.py

--- leetcode22_subarraySum.py
+++ leetcode22_subarraySum.py
@@ -7,32 +7,6 @@
 from collections import defaultdict
 def subarraySum(nums,k):
     count = 0
-    # for i in range(len(nums)):
-    #     for j in range(len(nums)):
-    #         if sum(nums[i:j+1]) == k:
-    #             print(nums[i:j+1])
-    #             count+=1
-    # return count
-
-    # cummulative_sum = [0]*(len(nums)+1)
-    # for i in range(1,len(nums)+1):
-    #     cummulative_sum[i] = cummulative_sum[i - 1] + nums[i - 1]
-    # print(cummulative_sum)
-    # for start in range(len(nums)):
-    #     for end in range(1,len(nums)+1):
-    #         if cummulative_sum[end] - cummulative_sum[start] == k:
-    #             count+=1
-    # return count
-
-    # for i in range(len(nums)):
-    #     s = 0
-    #     for j in range(i,len(nums)):
-    #         s+=nums[j]
-    #         print(s)
-    #         if s == k:
-    #             count+=1
-    # return count
-
     d = defaultdict(int)
     s = 0
     for i in range(len(nums)):
